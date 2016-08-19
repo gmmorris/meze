f4e235ebdf10bf908e2582e1615079dca3612aa8
Gidi Meir Morris <github@gidi.io>
2016-08-19 01:06:30 +0100

    refactored promise flattening

--- src/FlattenPromises.js
+++ src/FlattenPromises.js
@@ -1,43 +1,76 @@
-function applyResolution (ref, key, promise) {
-  return new Promise((resolve, reject) => {
-    promise
-      .then(resolution => {
-        if (resolution instanceof Promise) {
-          applyResolution(ref, key, resolution)
-            .then(resolve)
-        } else {
-          ref[key] = resolution
-          resolve(ref[key])
-        }
-      })
-      .catch(err => reject(err))
+import isPlainObject from 'lodash.isplainobject'
+import symbolPainer from './symbolPainter'
+
+function consoleLog () {
+  // console.log(...arguments)
+}
+
+function isPromsie (obj) {
+  return obj && !isPlainObject(obj) && (
+    obj instanceof Promise ||
+    Object.getPrototypeOf(obj) === Promise ||
+    typeof obj.then === 'function'
+  )
+}
+
+const { paint, painted, clean } = symbolPainer('safe')
+
+const applyResolution = msg => (ref, key, promise) => {
+  return promise.then(resolution => {
+    consoleLog(msg, 'key:', key)
+    ref[key] = resolution
+    return processResolution(ref[key])
   })
 }
 
-function flattenPromisesInObject (obj) {
-  // console.log(JSON.stringify(obj))
+const flattenPromisesInObject = msg => (obj) => {
+  painted(obj) ? consoleLog('shouldnt be parsed again') : null
+  consoleLog('obj:', JSON.stringify(obj))
   return Promise.all(Object
     .keys(obj)
     .reduce((promises, prop) => {
-      if (obj[prop] instanceof Promise) {
-        promises.push(applyResolution(obj, prop, obj[prop]))
-      } else if (obj[prop] instanceof Object) {
-        promises.push(applyResolution(obj, prop, flattenPromises(obj[prop])))
+      consoleLog(msg, 'prop:', prop)
+      if (isPromsie(obj[prop])) {
+        promises.push(applyResolution('applyPromise:')(obj, prop, obj[prop]))
+      } else if (isPlainObject(obj[prop])) {
+        promises.push(applyResolution('applyObj:')(obj, prop, flattenPromisesInObject(`in ${prop}:`)(obj[prop])))
       }
       return promises
-    }, [])).then(() => obj)
+    }, []))
+    .then(() => paint(obj))
+}
+
+function log (x, msg) {
+  consoleLog(...(msg ? [msg, x] : [x]))
+  return x
 }
 
 function flattenPromisesInPromise (promise) {
+  consoleLog('promise:', JSON.stringify(promise))
   return promise
-    .then(result => result instanceof Promise
-      ? flattenPromisesInPromise(result)
-      : result
-    )
+    .then(result => flattenPromises(result))
+}
+
+function processResolution (obj) {
+  (isPromsie(obj) ? consoleLog('is promise') : consoleLog('nope'))
+  return isPromsie(obj)
+      ? flattenPromisesInPromise(obj)
+      : (
+        isPlainObject(obj) && !painted(obj)
+        ? flattenPromisesInObject('in processResolution:')(obj)
+        : log(clean(obj), 'resolved:')
+      )
 }
 
 export default function flattenPromises (obj) {
-  return obj instanceof Promise
-    ? flattenPromises(flattenPromisesInPromise(obj))
-    : flattenPromisesInObject(obj)
+  return new Promise(resolve => {
+    const result = processResolution(obj)
+    if (isPromsie(result)) {
+      result.then(resolution => {
+        resolve(log(resolution, 'final:'))
+      })
+    } else {
+      resolve(result)
+    }
+  })
 }

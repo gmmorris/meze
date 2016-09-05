1967357c00cce6f7a3363a4f26218d5a74e540ce
Gidi Meir Morris <github@gidi.io>
2016-09-06 00:12:48 +0100

    added a test for child components requring promise flattening

--- packages/mezze/test/compose.js
+++ packages/mezze/test/compose.js
@@ -3,8 +3,10 @@ import test from 'ava'
 import { Component, createComponent } from '../modules/Component'
 import compose from '../modules/compose'
 
+import range from 'lodash.range'
+
 // compose
-test('flattenss a tree of components to get passed their internal promises', async t => {
+test('flattens a tree of components to get passed their internal promises', async t => {
   const Complex = Component(function (props) {
     const { left, right } = props
     const val = left < right
@@ -15,7 +17,7 @@ test('flattenss a tree of components to get passed their internal promises', asy
     return val
   })
 
-  const Root = Component(function (props) {
+  const Summarize = Component(function (props) {
     const { left, right } = props
     return {
       left,
@@ -25,7 +27,49 @@ test('flattenss a tree of components to get passed their internal promises', asy
   })
 
   t.deepEqual(
-    await compose(createComponent(Root, { left: 40, right: 50 })),
+    await compose(createComponent(Summarize, { left: 40, right: 50 })),
     { left: 40, right: 50, comparison: 'smaller' }
+  )
+})
+
+test('flattens child components into properties', async t => {
+  const PostponedSum = Component(function (props) {
+    const { left, right } = props
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve(left + right)
+      }, 10)
+    })
+  })
+
+  const Assign = Component(function (props) {
+    const { children } = props
+    return children.reduce((result, child, index) => {
+      result[index] = child
+      return result
+    }, {})
+  })
+
+  const Summarize = Component(function (props) {
+    const { left, right } = props
+    const rangeOfNumbers = range(left, right + 1)
+    return {
+      sum: createComponent(Assign, {},
+        ...rangeOfNumbers.map(index => createComponent(PostponedSum, { left, right: index }))
+      ),
+      count: rangeOfNumbers.length
+    }
+  })
+
+  t.deepEqual(
+    await compose(createComponent(Summarize, { left: 40, right: 42 })),
+    {
+      sum: {
+        0: 80,
+        1: 81,
+        2: 82
+      },
+      count: 3
+    }
   )
 })

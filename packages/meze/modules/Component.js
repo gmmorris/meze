398a1e3b6ff103b6a5e954d2afc784887ec5e180
Gidi Meir Morris <github@gidi.io>
2016-09-14 22:08:08 +0100

    added types to Component

--- packages/meze/modules/Component.js
+++ packages/meze/modules/Component.js
@@ -1,28 +1,36 @@
+/* @flow */
+
 import symbolPainter from './internals/symbolPainter'
 import ComponentInstance from './ComponentInstance'
 import isPlainObject from 'lodash.isplainobject'
 
+type ComponentPropType = Object
+type ComponentConstructorType = (props: ComponentPropType) => any
+type ComponentInstanciatorType =
+  (props: ComponentPropType) => any &
+  {
+    constructor: ComponentConstructorType
+  }
+
 const { paint, painted } = symbolPainter('Component')
 
 export const isComponent =
-  component => component && painted(component)
+  (component : any) : boolean => component && painted(component)
 
-const valdiateProps = props => {
+const valdiateProps = (props : ComponentPropType) : ComponentPropType => {
   if (!isPlainObject(props)) {
-    throw new Error(`Invalid props: ${props}`)
+    throw new Error(`Invalid props`)
   }
+  return props
 }
 
-export function Component (constructor) {
-  function instanciate (props = {}) {
-    valdiateProps(props)
+export function Component (constructor : ComponentConstructorType) : ComponentInstanciatorType {
+  function instanciate (props : ComponentPropType = {}) {
     // console.log(`instanciate`)
     // console.log(props)
-    return new ComponentInstance(constructor, props)
+    return new ComponentInstance(constructor, valdiateProps(props))
   }
-  instanciate.constructor = constructor
-  paint(instanciate)
-  return instanciate
+  return paint(Object.assign(instanciate, { constructor }))
 }
 
 export default {

aa46bda8782c08b0214b3e93b90872e0ef7d928f
Gidi Meir Morris <github@gidi.io>
2016-11-07 23:58:30 +0000

    cleaned up demo

--- packages/banquet/modules/index.js
+++ packages/banquet/modules/index.js
@@ -1,13 +1,10 @@
 import Meze from 'meze'
+import fetchUrl from 'fetch-promise'
 
 import Server, { Use, Plugins } from './Server'
 import { On } from './EventHandling'
 import Methods from './Methods'
 import Response from './Response'
-
-const GetPersonalisedMessage = ({ name }) => {
-  return `hello ${name}, we're speaking via composition`
-}
 
 export default {
   Server,
@@ -18,8 +15,30 @@ export default {
   Response
 }
 
+const extractNameFromResponse = result => JSON.parse(result.buf.toString())[0]
+
+const GetPersonalisedMessage = ({ name }) => {
+  return `hello ${name}, we're speaking via composition`
+}
+
+const GetRandomName = ({ children }) => fetchUrl('http://namey.muffinlabs.com/name.json')
+  .then(result => {
+    const randomName = extractNameFromResponse(result)
+    return Meze.compose(<GetPersonalisedMessage name={randomName} />)
+  })
+
 Meze.compose(
   <Server>
+    <Methods.Get path="/hello">
+      <Response.Send>
+        <GetRandomName />
+      </Response.Send>
+    </Methods.Get>
+    <Methods.Head path="/hello">
+      <Response.Send>
+        <GetRandomName />
+      </Response.Send>
+    </Methods.Head>
     <Methods.Get path="/hello/:name">
       <Response.Send>
         <GetPersonalisedMessage />

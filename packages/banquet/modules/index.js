2e7541469d127a63fc03028f8a61b6a52411fc9e
Gidi Meir Morris <github@gidi.io>
2016-11-23 11:11:05 +0000

    removed example api from banquet source

--- packages/banquet/modules/index.js
+++ packages/banquet/modules/index.js
@@ -1,5 +1,3 @@
-import Meze from 'meze'
-import fetchUrl from 'fetch-promise'
 
 import Server, { Use, Plugins } from './Server'
 import { On } from './EventHandling'
@@ -14,44 +12,3 @@ export default {
   Methods,
   Response
 }
-
-const extractNameFromResponse = result => JSON.parse(result.buf.toString())[0]
-
-const GetPersonalisedMessage = ({ name }) => {
-  return `hello ${name}, we're speaking via composition`
-}
-
-const GetRandomName = ({ children }) => fetchUrl('http://namey.muffinlabs.com/name.json')
-  .then(result => {
-    const randomName = extractNameFromResponse(result)
-    return <GetPersonalisedMessage name={randomName} />
-  })
-
-Meze.compose(
-  <Server>
-    <Methods.Get path="/hello">
-      <Response.Send>
-        <GetRandomName />
-      </Response.Send>
-    </Methods.Get>
-    <Methods.Head path="/hello">
-      <Response.Send>
-        <GetRandomName />
-      </Response.Send>
-    </Methods.Head>
-    <Methods.Get path="/hello/:name">
-      <Response.Send>
-        <GetPersonalisedMessage />
-      </Response.Send>
-    </Methods.Get>
-    <Methods.Head path="/hello/:name">
-      <Response.Send>
-        <GetPersonalisedMessage />
-      </Response.Send>
-    </Methods.Head>
-  </Server>
-).then((server) => {
-  server.listen(8080, function () {
-    console.log('%s listening at %s', server.name, server.url)
-  })
-})

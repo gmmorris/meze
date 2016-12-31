0f0d16804d38c309bff050645b81c835b4aa7d25
Gidi Meir Morris <github@gidi.io>
2016-12-31 13:48:58 +0000

    forgot filter export

--- packages/meze/modules/children/index.js
+++ packages/meze/modules/children/index.js
@@ -2,9 +2,11 @@
 import mapping from './mapping'
 import reduction from './reduction'
 import only from './only'
+import filter from './filter'
 
 export default {
   ...mapping,
   ...reduction,
-  ...only
+  ...only,
+  ...filter
 }
